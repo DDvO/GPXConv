33a72238d76b7c71af920a1186b1a59190267a00
Dr. David von Oheimb <dev@ddvo.net>
2025-01-11 07:09:15 +0100

    add test_merge

--- test.sh
+++ test.sh
@@ -22,3 +22,7 @@ gpxconv $GPXCONV_TEST_ARGS test.out2 >test.out3 2>test.log3;
 gpxconv test_sanitization.gpx >test_sanitization.out 2>test_sanitization.log;
     diff test_sanitization{_reference,}.out;
     diff test_sanitization{_reference,}.log
+
+gpxconv test_merge1.gpx -merge test_merge2.gpx >test_merge.out 2>test_merge.log;
+    diff test_merge{_reference,}.out;
+    diff test_merge{_reference,}.log
