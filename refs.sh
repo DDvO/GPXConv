5a247851c796d2a200dd01008f7f3c9322430f86
Dr. David von Oheimb <dev@ddvo.net>
2025-02-01 18:11:03 +0100

    add refs.sh for conveniently updating the test reference log and output files

---
+++ refs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mv test_minimal{,_reference}.log
+mv test_minimal{,_reference}.out
+
+mv test{,_reference}.log
+mv test{,_reference}.out
+
+mv test_sanitization{,_reference}.log
+mv test_sanitization{,_reference}.out
+
+mv test_merge{,_reference}.log
+mv test_merge{,_reference}.out
+mv test_merge{,_reference}_out_1.gpx
+mv test_merge{,_reference}_out_2.gpx
