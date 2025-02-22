f6cc131705b6430b981dbd1e8d461ec40a1dc4af
Dr. David von Oheimb <dev@ddvo.net>
2025-02-22 19:24:36 +0100

    test.sh etc.: use test_minimal.gpx also with -merge

--- refs.sh
+++ refs.sh
@@ -3,6 +3,9 @@
 mv test_minimal{,_reference}.log
 mv test_minimal{,_reference}.out
 
+mv test_minimal_merge{,_reference}.log
+mv test_minimal_merge{,_reference}.out
+
 mv test{,_reference}.log
 mv test{,_reference}.out
 
--- test.sh
+++ test.sh
@@ -10,6 +10,12 @@ gpxconv test_minimal.out2 >test_minimal.out3 2>test_minimal.log3;
     diff test_minimal.out{2,3};
     diff test_minimal.log{2,3}
 
+gpxconv -info_wpts -warn_wpts test_minimal.gpx -merge test_minimal.gpx >test_minimal_merge.out  2>test_minimal_merge.log;
+    diff test_minimal_merge{_reference,}.out;
+    diff test_minimal_merge{_reference,}.log
+#   diff test_minimal{,_merge}_reference.out
+#   diff test_minimal{,_merge}_reference.log
+
 echo -e "\n### test, initial pass #############################################"
 export GPXCONV_TEST_ARGS="-walk -min_ext value 101 -max_ext value 101 -smooth -weight 50 -phases -segs 2..4+100 -cmt_wpts elevation -src_wpts substituted|inserted"
 gpxconv $GPXCONV_TEST_ARGS test.gpx  >test.out  2>test.log;
@@ -18,7 +24,7 @@ gpxconv $GPXCONV_TEST_ARGS test.gpx  >test.out  2>test.log;
 gpxconv $GPXCONV_TEST_ARGS test.out  >test.out2 2>test.log2
 gpxconv $GPXCONV_TEST_ARGS test.out2 >test.out3 2>test.log3;
 # 3rd iteration should not lead to changes compared to 2nd one:
-echo -e "\n### test, after three iterations #####################################"
+echo -e "\n### test, after three iterations ###################################"
     diff -w test.out{2,3};
     diff -w test.log{2,3}
 
